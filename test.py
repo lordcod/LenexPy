ac6cf1254eac838be7f066665a760f5dc353fd40
LordCode <9999269010dddd@gmail.com>
2026-01-23 21:22:34 +0300

    up

--- test.py
+++ test.py
@@ -1,4 +1,15 @@
+from pathlib import Path
 from lenexpy import fromfile, tofile
-path = r"C:\Users\2008d\Downloads\20260125_Брасс.lxf"
+from lenexpy.models.heat import Heat
+from lenexpy.models.meet import Meet
 
-tofile(fromfile(path), "output.xml")
+
+filename = r"C:\Users\2008d\working-space\LenexPy\meet.xml"
+path = Path(filename)
+with open(path, "rb") as file:
+    meet = Meet.from_xml(file.read())
+
+filename = r"C:\Users\2008d\working-space\LenexPy\heat.xml"
+path = Path(filename)
+with open(path, "rb") as file:
+    heat = Heat.from_xml(file.read())
